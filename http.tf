bb3491907566469817c5c9117f48092e0e10d180
Evandro Myller <emyller@d7.dev>
2022-02-04 20:19:15 -0300

    Keep only generic rules last in the list

--- http.tf
+++ http.tf
@@ -30,16 +30,6 @@ locals {
     ]): ("${rule.service_name}|${rule.hostname}|${rule.path}") => rule
   }
 
-  # Split rule combinations in specific and generic (path = *) ones
-  load_balancer_rules_combinations_with_path = [
-    for rule in values(local.load_balancer_rules_combinations):
-    rule if rule.path != "*"
-  ]
-  load_balancer_rules_combinations_without_path = [
-    for rule in values(local.load_balancer_rules_combinations):
-    rule if rule.path == "*"
-  ]
-
   # Standard limit of rules per ALB listener
   rules_count_limit = 100
 
@@ -50,12 +40,6 @@ locals {
   listener_arns = {
     for service_name, service in local.http_services:
     service_name => data.aws_lb_listener.https[service.http.load_balancer_name].arn
-  }
-
-  # Map of listener to a rule priority block start
-  listener_rule_priority_blocks = {
-    for lb_name in local.load_balancer_names:
-    lb_name => (random_integer.rule_priority_block_start[lb_name].result * local.rules_count_limit)
   }
 }
 
@@ -99,13 +83,9 @@ resource "aws_lb_listener_rule" "main" {
   listener_arn = local.listener_arns[each.value.service_name]
 
   # Assign a priority according to specificity
-  priority = sum([
-    local.listener_rule_priority_blocks[each.value.load_balancer_name],
-    (each.value.path == "*"
-      ? local.rules_count_limit - index(local.load_balancer_rules_combinations_without_path, each.value)
-      : index(local.load_balancer_rules_combinations_with_path, each.value)
-    ),
-  ])
+  priority = each.value.path == "*" ? (
+    random_integer.rule_priority_star_path[each.key].result
+  ) : null
 
   action {
     type = "forward"
@@ -121,20 +101,22 @@ resource "aws_lb_listener_rule" "main" {
   }
 }
 
-resource "random_integer" "rule_priority_block_start" {
+resource "random_integer" "rule_priority_star_path" {
   /*
   A number to manage the priority numbers to use in ALB rules
 
   ALBv2 does not sort rules by specificity; rather, it sorts them by a priority
   number from 1 to 50000. Also, the default limit of rules per listener is 100.
-  This resource manages a number multiple of 100 to serve as a block of
-  available priorities to use in the load balancer rules, having the intention
-  of keeping the most generic ones last.
+  This resource manages a random number in the last 30000 positions to try and
+  keep generic rules (path = *) last in the list of rules.
 
   More: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-update-rules.html
   */
-  for_each = local.load_balancer_names
-  min = 0  # first block: 0-100
-  max = 499  # last block: 49900-50000
-  keepers = { listener_arn = data.aws_lb_listener.https[each.key].arn }
+  for_each = {
+    for rule_id, rule in local.load_balancer_rules_combinations:
+    rule_id => rule if rule.path == "*"  # Rules without a path defined
+  }
+  min = 20000
+  max = 50000
+  keepers = { listener_arn = data.aws_lb_listener.https[each.value.load_balancer_name].arn }
 }
