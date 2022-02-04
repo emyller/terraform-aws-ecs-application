138f011d4878343be313929e6de566182be64cdc
Evandro Myller <emyller@d7.dev>
2022-02-04 19:00:47 -0300

    Manage load balancer rule priorities

--- http.tf
+++ http.tf
@@ -11,6 +11,46 @@ locals {
     for service_name in keys(var.services):
     service_name => "${local.common_name}-${service_name}"
   }
+
+  # Create a recursive product of service_name + hostname + path for ALB rules
+  load_balancer_rules_combinations = {
+    for rule in flatten([
+      for service_name, service in local.http_services: [
+        for combo in setproduct(
+          [service_name],  # service_name
+          service.http.hostnames,  # hostname
+          coalesce(service.http.paths, ["*"]),  # path -- default to "*"
+        ): {
+          service_name = combo[0]
+          hostname = combo[1]
+          path = combo[2]
+          listener_arn = local.http_services[combo[0]].http.listener_arn
+        }
+      ]
+    ]): ("${rule.service_name}|${rule.hostname}|${rule.path}") => rule
+  }
+
+  # Split rule combinations in specific and generic (path = *) ones
+  load_balancer_rules_combinations_with_path = [
+    for rule in values(local.load_balancer_rules_combinations):
+    rule if rule.path != "*"
+  ]
+  load_balancer_rules_combinations_without_path = [
+    for rule in values(local.load_balancer_rules_combinations):
+    rule if rule.path == "*"
+  ]
+
+  # Standard limit of rules per ALB listener
+  rules_count_limit = 100
+
+  # Distinct load balancer listeners
+  listeners_arns = distinct(values(local.http_services)[*].http.listener_arn)
+
+  # Map of listener to a rule priority block start
+  listener_rule_priority_blocks = {
+    for listener_arn in local.listeners_arns:
+    listener_arn => (random_integer.rule_priority_block_start[listener_arn].result * local.rules_count_limit)
+  }
 }
 
 resource "aws_lb_target_group" "http" {
@@ -37,33 +77,18 @@ resource "aws_lb_target_group" "http" {
 resource "aws_lb_listener_rule" "main" {
   /*
   Rules for the load balancer listener
-
-  Mapped by a product of services + hostnames + paths, e.g:
-  {
-    "service1-example.com-*": {
-      service_name = "service1", hostname = "example.com", path = "*"
-    },
-    "service1-example.com-/foo": {
-      service_name = "service1", hostname = "example.com", path = "/foo"
-    },
-  }
-
-  TODO: manage order with rule priority
   */
-  for_each = {
-    for rule in flatten([
-      for service_name, service in local.http_services: [
-        for combo in setproduct(
-          [service_name],
-          service.http.hostnames,
-          coalesce(service.http.paths, ["*"]),  # Default to * (any path)
-        ):
-        zipmap(["service_name", "hostname", "path"], combo)
-      ]
-    ]): ("${rule.service_name}-${rule.hostname}-${rule.path}") => rule
-  }
+  for_each = local.load_balancer_rules_combinations
+  listener_arn = each.value.listener_arn
 
-  listener_arn = local.http_services[each.value.service_name].http.listener_arn
+  # Assign a priority according to specificity
+  priority = sum([
+    local.listener_rule_priority_blocks[each.value.listener_arn],
+    (each.value.path == "*"
+      ? local.rules_count_limit - index(local.load_balancer_rules_combinations_without_path, each.value)
+      : index(local.load_balancer_rules_combinations_with_path, each.value)
+    ),
+  ])
 
   action {
     type = "forward"
@@ -77,4 +102,22 @@ resource "aws_lb_listener_rule" "main" {
   condition {
     path_pattern { values = [each.value.path] }
   }
+}
+
+resource "random_integer" "rule_priority_block_start" {
+  /*
+  A number to manage the priority numbers to use in ALB rules
+
+  ALBv2 does not sort rules by specificity; rather, it sorts them by a priority
+  number from 1 to 50000. Also, the default limit of rules per listener is 100.
+  This resource manages a number multiple of 100 to serve as a block of
+  available priorities to use in the load balancer rules, having the intention
+  of keeping the most generic ones last.
+
+  More: https://docs.aws.amazon.com/elasticloadbalancing/latest/application/listener-update-rules.html
+  */
+  for_each = toset(local.listeners_arns)
+  min = 0  # first block: 0-100
+  max = 499  # last block: 49900-50000
+  keepers = { listener_arn = each.key }
 }
