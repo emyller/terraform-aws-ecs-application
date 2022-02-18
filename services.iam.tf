83e451fddba7683e0b4369efad002d7e2650528c
Evandro Myller <emyller@d7.dev>
2022-02-18 00:13:10 -0300

    Improve IAM role name

--- services.iam.tf
+++ services.iam.tf
@@ -7,7 +7,7 @@ resource "aws_iam_role" "ecs_agent" {
   - Fetch secrets from Secrets Manager and inject them in containers as
     environment variables (if any).
   */
-  name = "ecs-${var.application_name}-${var.environment_name}"
+  name = "ecs-${var.environment_name}-${var.application_name}"
   assume_role_policy = data.aws_iam_policy_document.ecs_agent_assume.json
 }
 
